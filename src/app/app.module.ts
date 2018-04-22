23883e72c5098dd3fe28dfb8869ce65d3aeaa735
Shashikant-Maharshi <maharshishashikant@gmail.com>
2018-04-22 13:15:33 +0530

    Integrated prime-ng UI component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
 
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxElectronModule],
+  imports: [BrowserModule, BrowserAnimationsModule, NgxElectronModule],
   providers: [],
   bootstrap: [AppComponent]
 })
